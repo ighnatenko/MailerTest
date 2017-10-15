a14c2fb58ed9eac5d1f20c7d63ede003d421ab99
Alexandr <ighnatenko_alexandr@meta.ua>
2017-10-15 20:31:38 +0300

    .

--- config/application.rb
+++ config/application.rb
@@ -32,12 +32,12 @@ module MailerTest
     ActionMailer::Base.smtp_settings = {
 		  :user_name => ENV['SENDGRID_USERNAME'],
 		  :password => ENV['SENDGRID_PASSWORD'],
-		  :domain => 'yourdomain.com',
+		  :domain => 'herokuapp.com',
 		  :address => 'smtp.sendgrid.net',
 		  :port => 587,
 		  :authentication => :plain,
 		  :enable_starttls_auto => true
 		}
-		
+
   end
 end
